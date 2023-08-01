62f6a8a63aaece3962b0914e3faf52615cc45c88
Jhon-mantila <kaiser_4tu@hotmail.com>
2023-07-31 21:14:36 -0500

    Validacion de requerios y no perder la información cuando se recarga

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -23,6 +23,11 @@ class PostController extends Controller
 
     public function store(Request $request){
         //dd($request);
+        $request->validate([
+            'title' => 'required',
+            'body'  => 'required',
+        ]);
+
         $post = $request->user()->posts()->create([
             'title' => $title = $request->title,
             'slug' => Str::slug($title),
@@ -41,6 +46,10 @@ class PostController extends Controller
 
     public function update(Request $request, Post $post){
         //dd($request);
+        $request->validate([
+            'title' => 'required',
+            'body'  => 'required',
+        ]);
         $post->update([
             'title' => $title = $request->title,
             'slug' => Str::slug($title),
